317bec78fe333714277491d7a8a24455d99ec6a1
RahafAlatiq <439201283@student.ksu.edu.sa>
2022-11-09 01:24:19 +0300

    Add python code for models

---
+++ python/numbers/CNN/number_testing.py
@@ -0,0 +1,99 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+from cvzone.ClassificationModule import Classifier
+from tensorflow import keras
+
+
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+#remove comment to test model
+
+#Model 1 : CNN : 
+classifier = keras.models.load_model("CNN/number_model.h5")
+labels = ['zero','one','two','three','four', 'five', 'six', 'seven', 'eight', 'nine']
+
+#Model 2 : Teachable Machine model:
+#classifier = Classifier("Teachable_Machine/converted_keras/keras_model.h5", "Teachable_Machine/converted_keras/labels.txt")
+#labels = ['zero','one','two','three','four', 'five', 'six', 'seven', 'eight', 'nine']
+
+
+
+
+
+
+offset = 25
+imgSize = 300
+   
+while True:
+    success, img = cap.read()
+    i = img.copy()
+    imgOutput = img.copy()
+    hands, img = detector.findHands(img)
+    
+    try:
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand['bbox']
+           
+ 
+            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+            
+            imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+
+            imgCropShape = imgCrop.shape
+            
+            aspectRatio = h / w
+            
+            if aspectRatio > 1:
+               k = imgSize / h
+               wCal = math.ceil(k * w)
+               imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+               imgResizeShape = imgResize.shape
+               wGap = math.ceil((imgSize - wCal) / 2)
+               imgWhite[:, wGap:wCal + wGap] = imgResize
+               img_gray = imgWhite.copy()
+               img_gray = cv2.resize(img_gray, (224,224))
+               i = img_gray.reshape(1,224,224,3)
+               prediction = classifier.predict(i)
+               index = prediction.argmax(axis=-1)
+               print("------------------------------------------------")
+               print(labels[index[0]])
+
+            
+            else:
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                imgResizeShape = imgResize.shape
+                hGap = math.ceil((imgSize - hCal) / 2)
+                imgWhite[hGap:hCal + hGap, :] = imgResize
+                img_gray = imgWhite.copy()
+                img_gray = cv2.resize(img_gray, (224,224))
+                i = img_gray.reshape(1,224,224,3)
+                prediction = classifier.predict(i)
+                index = prediction.argmax(axis=-1)
+                print("------------------------------------------------")
+                print(labels[index[0]])
+
+
+            cv2.rectangle(imgOutput, (x - offset, y - offset-50),(x - offset+90, y - offset-50+50), (255, 0, 255), cv2.FILLED)
+            cv2.putText(imgOutput, labels[index[0]], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+            cv2.rectangle(imgOutput, (x-offset, y-offset),(x + w+offset, y + h+offset), (255, 0, 255), 4)
+
+            #cv2.imshow("Cropp", imgCrop)
+            #cv2.imshow("imgWhite", imgWhite)
+    
+    except Exception as e:
+        print(str(e))
+        pass
+
+    cv2.imshow("Image", imgOutput)
+    key = cv2.waitKey(1)
+    if key == ord("a"): 
+        cap.release()
+        cv2.destroyAllWindows()
+        
